41e850b36f48fa515cc09ee88751944bd45e76ee
ilo5u <livewithoutsacrifice@outlook.com>
2020-03-28 14:48:20 +0800

    build the pipeline

--- sys/sys.srcs/sources_1/new/emit.v
+++ sys/sys.srcs/sources_1/new/emit.v
@@ -5,7 +5,7 @@
 // 
 // Create Date: 2020/03/26
 // Design Name: 
-// Module Name: chinx_accu
+// Module Name: chinx_emit
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -23,24 +23,27 @@
 module chinx_emit(
     input wire clk,
     input wire rst,
-    input wire busy,
-    input wire flush,
 
     input wire [`ADDR_WIDTH - 1:0] pc_i,
     input wire [`INS_WIDTH - 1:0] instr_i,
-
+    // from HI/LO register
+    input wire [`DATA_WIDTH - 1:0] hi_i,
+    input wire [`DATA_WIDTH - 1:0] lo_i,
+    // from load operation
+    input wire [`DATA_WIDTH - 1:0] load_i,
+    // write back to regfiles
     output reg [`REG_ADDR_WIDTH - 1:0] waddr_o,
     output reg [`REG_ADDR_WIDTH - 1:0] raddr0_o,
     output reg [`REG_ADDR_WIDTH - 1:0] raddr1_o,
-
+    // handle the 16-bit immediate number
     output reg [`INSTR_IMM_WIDTH - 1:0] imm_o,
-    output reg [`INSTR_BADDR_WIDTH - 1:0] baddr_o,
-
     output reg immext_o, // signed or unsigned
 
     output reg wbe_o, // write back to register files
-    output reg memce_o, // need MEM stage
+    output reg hle_o, // write back to HI/LO register
+    output reg memce_o, // need MEM stage [validate for 2 periods]
     output reg memop_o, // load or store memory
+    output reg [`MEM_OPND_WIDTH - 1:0] memod_o, // word, half, halfu, byte, byteu
 
     output reg [`ALU_SRC_WIDTH - 1:0] alusrca_o, // Rb, Rc, ExtImm
     output reg [`ALU_SRC_WIDTH - 1:0] alusrcb_o, // Rb, Rc, ExtImm
@@ -52,6 +55,8 @@ module chinx_emit(
     // Branch
     output reg be_o,
 
+    // stall the pipeline
+    // by sending this signal to stage1
     output reg stall
 );
 
@@ -86,107 +91,136 @@ wire [`REG_ADDR_WIDTH - 1:0] rc_w = instr_i[15:11];
 wire [`INSTR_IMM_WIDTH - 1:0] imm_w = instr_i[15:0];
 wire [`INSTR_BADDR_WIDTH - 1:0] baddr_w = instr_i[25:0];
 
+// handle conflicts when emit 
+// parallel operations with load/store operation
 reg [`REG_ADDR_WIDTH - 1:0] conflict_r; // used for lw and mul operation
+reg ls_busy_r;
+reg ls_period_r;
 
-reg load_r;
+// handle branch effects
+reg br_flush_r;
 
 always @(posedge clk) begin
     if (rst == `LEV_H) begin
         // output signals
+        waddr_o <= `REG_RA;
+        raddr0_o <= `REG_ZERO;
+        raddr1_o <= `REG_ZERO;
         imm_o <= `INSTR_IMM_WIDTH'd0;
-        baddr_o <= `INSTR_BADDR_WIDTH'd0;
-        immext_o <= `LEV_L;
+        immext_o <= `EXT_SIGNED;
         wbe_o <= `LEV_L;
+        hle_o <= `LEV_L;
         memce_o <= `LEV_L;
         memop_o <= `MEM_LOAD;
-
+        memod_o <= `MEM_OPND_WORD;
+        alusrca_o <= `ALU_SRC_R0;
+        alusrcb_o <= `ALU_SRC_R1;
+        alures_o <= `ALU_RES_ADD;
+        be_o <= `LEV_L;
+        stall <= `LEV_L;
         // internal signals
-        conflict_r <= `REG_ADDR_WIDTH'd0;
-    end else if (flush == `LEV_H) begin
+        conflict_r <= `REG_ZERO;
+        ls_busy_r <= `LEV_L;
+        ls_period_r <= `LEV_L;
+        br_flush_r <= `LEV_L;
+    // end else if (flush == `LEV_H) begin
+    //     // dismiss the write effects
+    //     wbe_o <= `LEV_L;
+    end else if (br_flush_r == `LEV_H) begin
+        br_flush_r <= `LEV_L;
         // dismiss the write effects
         wbe_o <= `LEV_L;
+        be_o <= `LEV_L;
     end else begin
         imm_o <= imm_w;
         if ((opcode_w > ALO_BEGIN) && (opcode_w < ALO_END)) begin
+            ls_period_r <= ~ls_period_r; // used when load/store is running
             // add,addi,ori
             // set the data flow from regfiles
-            waddr_o <= ra_w;
+            waddr_o <= (ls_busy_r == `LEV_H && ls_period_r == `LEV_H) ? // load
+                conflict_r : ra_w;
             raddr0_o <= rb_w;
             raddr1_o <= rc_w;
             // all operations need to write back
             wbe_o <= `LEV_H;
             // no load and store operations
             memce_o <= `LEV_L;
-            memop_o <= `MEM_LOAD;
-
             // set the extension model of immediate number
             immext_o <= (opcode_w < ALO_UIMM) ? `EXT_SIGNED : `EXT_UNSIGNED;
-
-            // set the data flow sources of ALU
+            // set the data flow of sources sent to ALU
             alusrca_o <= `ALU_SRC_R0;
             alusrcb_o <= (opcode_w < ALO_SIMM) ? `ALU_SRC_R1 : `ALU_SRC_IMM;
-
+            // set the data flow of ALU result
             alures_o <= (opcode_w == ADD || opcode_w == ADDI) ?
                 `ALU_RES_ADD : `ALU_RES_OR;
-
             // do not branch
             be_o <= `LEV_L;
-            
-            load_r <= ~load_r;
-            if (busy == `LEV_H && load_r == `LEV_H) begin
+            if (ls_busy_r == `LEV_H && ls_period_r == `LEV_H) begin
                 // load operation will occupy the WB stage
+                // stall the stage1 to keep the pc value
                 stall <= `LEV_H;
-            end else if (busy == `LEV_H && ra_w == conflict_r ) begin
+                ls_busy_r <= `LEV_L;
+            end else if (ls_busy_r == `LEV_H && ra_w == conflict_r ) begin
                 // WAW conflict
+                // stall the stage1 to keep the pc value
                 stall <= `LEV_H;
             end else begin
-                stall <= `LEV_L; // no load or store or multiply operations before
+                stall <= `LEV_L; // no load/store operations before or no conflict
                 conflict_r <= `REG_ADDR_WIDTH'd0; // release
             end
         end else if ((opcode_w > BRO_BEGIN) && (opcode_w < BRO_END)) begin
+            ls_period_r <= ~ls_period_r;
+            ls_busy_r <= (ls_busy_r == `LEV_H && ls_period_r == `LEV_H) ?
+                `LEV_L : ls_busy_r;
             // ret,jr
             // no operations need to write back, but pc register in Co-processor
-            wbe_o <= `LEV_L;
+            // except that the load operation is done
+            wbe_o <= (ls_busy_r == `LEV_H && ls_period_r == `LEV_H) ? // load
+                `LEV_H : `LEV_L;
+            waddr_o <= conflict_r;
             // no load and store operations
             memce_o <= `LEV_L;
-            memop_o <= `MEM_LOAD;
-
             // set the extension model of immediate number
             immext_o <= `EXT_SIGNED;
-
             // set the data flow sources and result of ALU
             alusrca_o <= `ALU_SRC_R1;
             alusrcb_o <= `ALU_SRC_IMM;
             alures_o <= `ALU_RES_ADD;
-
             // branch
             be_o <= `LEV_H;
+            br_flush_r <= `LEV_H;
         end else if ((opcode_w > LSO_BEGIN) && (opcode_w < LSO_END)) begin
             // lw,sw
             // load operations need to write back
-            wbe_o <= (opcode_w < LSO_STORE) ? `LEV_H : `LEV_L;
+            // wbe_o <= (opcode_w < LSO_STORE) ? `LEV_H : `LEV_L;
+            // but not the next period
+            // except that the last load operation is done
+            wbe_o <= (ls_busy_r == `LEV_H && ls_period_r == `LEV_H) ? // load
+                `LEV_H : `LEV_L;
+            waddr_o <= conflict_r;
             // load and store operations
             memce_o <= `LEV_H;
             memop_o <= (opcode_w < LSO_STORE) ? `MEM_LOAD : `MEM_STORE;
-
+            memod_o <= `MEM_OPND_WORD;
             // set the extension model of immediate number
             immext_o <= `EXT_SIGNED;
-
             // set the data flow sources and result of ALU
             alusrca_o <= `ALU_SRC_R1;
             alusrcb_o <= `ALU_SRC_IMM;
             alures_o <= `ALU_RES_ADD;
-
             // do not branch
             be_o <= `LEV_L;
-            
-            if (busy == `LEV_H) begin
+            if (ls_busy_r == `LEV_H && ls_period_r == `LEV_L) begin
+                // the last load/store is runnig now
                 stall <= `LEV_H;
+                ls_period_r = `LEV_H;
             end else begin
-                stall <= `LEV_L; // no load or store or multiply operations before
+                // the last load/store operation is done
+                stall <= `LEV_L;
                 conflict_r <= (opcode_w < LSO_STORE) ? 
                     `REG_ADDR_WIDTH'd0 : ra_w; // release
-                load_r <= `LEV_L;
+                ls_period_r <= `LEV_L;
+                ls_busy_r <= `LEV_H;
             end
         end
     end
--- sys/sys.srcs/sources_1/new/stage2.v
+++ sys/sys.srcs/sources_1/new/stage2.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 2020/03/27 15:23:33
+// Create Date: 2020/03/27
 // Design Name: 
 // Module Name: chinx_stage2
 // Project Name: 
@@ -23,7 +23,6 @@
 module chinx_stage2(
     input clk,
     input mulclk,
-    input ramclk,
     input rst,
 
     input wire [`ADDR_WIDTH - 1:0] pc_i,
@@ -33,11 +32,99 @@ module chinx_stage2(
 
     output wire [`ADDR_WIDTH - 1:0] addr_o,
     output wire [`DATA_WIDTH - 1:0] store_o,
+
+    output wire memce_o,
+    output wire memop_o,
+    output wire memod_w,
+    
+    output wire be_o,
+    output wire [`ADDR_WIDTH - 1:0] baddr_o,
     output wire stall
 );
 
+wire [`DATA_WIDTH - 1:0] hio_w;
+wire [`DATA_WIDTH - 1:0] loo_w;
+wire hle_w;
+wire [`REG_ADDR_WIDTH - 1:0] waddr_w;
+wire [`REG_ADDR_WIDTH - 1:0] raddr0_w;
+wire [`REG_ADDR_WIDTH - 1:0] raddr1_w;
+wire [`INSTR_IMM_WIDTH - 1:0] imm16_w;
+wire immext_w;
+wire wbe_w;
+wire [`ALU_SRC_WIDTH - 1:0] alusrca_w;
+wire [`ALU_SRC_WIDTH - 1:0] alusrcb_w;
+wire [`ALU_RES_WIDTH - 1:0] alures_w;
 chinx_emit emit(
-    
+    .clk(clk),
+    .rst(rst),
+    .pc_i(pc_i),
+    .instr_i(instr_i),
+    .hi_i(hio_w),
+    .lo_i(loo_w),
+    .load_i(load_i),
+    .waddr_o(waddr_w),
+    .raddr0_o(raddr0_w),
+    .raddr1_o(raddr1_w),
+    .imm_o(imm16_w),
+    .immext_o(immext_w),
+    .wbe_o(wbe_w),
+    .hle_o(hle_w),
+    .memce_o(memce_o),
+    .memop_o(memop_o),
+    .memod_o(memod_o),
+    .alusrca_o(alusrca_w),
+    .alusrcb_o(alusrcb_w),
+    .alures_o(alures_w),
+    .be_o(be_o)，
+    .stall(stall)
 );
+
+wire [`DATA_WIDTH - 1:0] rdata0_w;
+wire [`DATA_WIDTH - 1:0] rdata1_w;
+chinx_regfiles regfiles(
+    .clk(clk),
+    .rst(rst),
+    .raddr0_i(raddr0_w),
+    .raddr1_i(raddr1_w),
+    .we_i(wbe_w),
+    .waddr_i(waddr_w),
+    .rdata0_o(rdata0_w),
+    .rdata1_o(rdata1_w)
+);
+
+wire [`DATA_WIDTH - 1:0] imm32_w;
+chinx_ext32 ext32(
+    .immext_i(immext_w),
+    .imm16_i(imm16_w),
+    .imm32_o(imm32_w)
+);
+
+wire [`DATA_WIDTH - 1:0] result_w;
+wire [`DATA_WIDTH - 1:0] hi_w;
+chinx_alu alu(
+    .mulclk(mulclk),
+    .alusrca_i(alusrca_w),
+    .alusrcb_i(alusrcb_w),
+    .rdata0_i(rdata0_w),
+    .rdata1_i(rdata1_w),
+    .extimm_i(imm32_w),
+    .pc_i(pc_i),
+    .alures_i(alures_w),
+    .result_o(result_w),
+    .hi_o(hi_w)
+);
+
+chinx_hilo hilo(
+    .clk(clk),
+    .we(hle_w),
+    .hi_i(hi_w),
+    .lo_i(result_w),
+    .hi_o(hio_w),
+    .lo_o(loo_w)
+);
+
+assign addr_o = result_w;
+assign store_o = rdata0_w;
+assign baddr_o = result_w[15:0];
 
 endmodule
